92b80d1c95123532a6032698be5384a100d02032
Семин Иван <i.semin@zniis.ru>
2021-02-15 22:36:11 +0300

    add pulse graph test

--- temperature-sensor/src/index.js
+++ temperature-sensor/src/index.js
@@ -1,38 +1,31 @@
-var ctx = document.getElementById('myChart').getContext('2d');
+const color = "red";
 
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+const options = {};
+
+const timeline = ["1", "2", "3", "1", "2", "3", "1", "2", "3"];
+
+const data = [1, 2, 3, 1, 7];
+
+const config = {
+  type: "line",
+  data: {
+    labels: timeline,
+    datasets: [
+      {
+        label: "Pulse",
+        data,
+        fill: false,
+        backgroundColor: color,
+        borderColor: color,
+      },
+    ],
+  },
+  options,
+};
+
+window.onload = function () {
+  const ctx = document.getElementById("chart").getContext("2d");
+  window.chart = new Chart(ctx, config);
+};
+
+// window.chart.update();
